eab237b174b8245f2db6636b4067df800564bac8
s-jaswal <sjasdocs@gmail.com>
2024-09-22 20:44:39 -0400

    did the javascript part

--- script.js
+++ script.js
@@ -2,3 +2,24 @@
 isnpiration
 https://cz.pinterest.com/pin/830703093794665464/
 */
+
+function slidesPlugin(activeSlide = 0) {
+    const slides = document.querySelectorAll(".slide");
+
+    slides[activeSlide].classList.add("active");
+
+    for (const slide of slides) {
+        slide.addEventListener("click", () => {
+            clearActiveClasses();
+            slide.classList.add("active");
+        });
+    }
+
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove("active");
+        });
+    }
+}
+
+slidesPlugin(2);
